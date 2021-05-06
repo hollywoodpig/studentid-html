3f7a4524c780b50dc627c09bb72c1ff6cda35264
hollywoodpig <nekitkld@gmail.com>
2021-05-06 13:29:00 +0300

    update

--- app/js/app.js
+++ app/js/app.js
@@ -1,18 +1,34 @@
-const typeTargets = document.querySelectorAll("[data-type]");
-const typeButtons = document.querySelectorAll("[data-type-open]");
+const typeTargets = document.querySelectorAll('[data-type]')
+const typeButtons = document.querySelectorAll('[data-type-open]')
 
-typeButtons.forEach((button) => {
-  button.addEventListener("click", (e) => {
-    e.preventDefault();
+typeButtons.forEach(button => {
+  button.addEventListener('click', e => {
+    e.preventDefault()
 
-    const buttonType = button.getAttribute("data-type-open");
+    const buttonType = button.getAttribute('data-type-open')
 
-    typeTargets.forEach((target) => {
-      target.removeAttribute("data-visible");
+    typeTargets.forEach(target => {
+      target.removeAttribute('data-visible')
 
-      const targetType = target.getAttribute("data-type");
+      const targetType = target.getAttribute('data-type')
 
-      if (buttonType === targetType) target.setAttribute("data-visible", true);
-    });
-  });
-});
+      if (buttonType === targetType) target.setAttribute('data-visible', true)
+    })
+  })
+})
+
+// temp
+
+const dataClicks = document.querySelectorAll('[data-click]')
+
+dataClicks.forEach(button => {
+  button.addEventListener('click', e => {
+    e.preventDefault()
+
+    dataClicks.forEach(button => {
+      button.classList.remove('active')
+    })
+
+    button.classList.add('active')
+  })
+})
